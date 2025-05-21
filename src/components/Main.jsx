372f63a75240ea5754e942129f542de5e950d3da
Freddie <119540226+SanJammin@users.noreply.github.com>
2025-05-21 11:23:04 +0100

    Finish main content and add styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,15 @@
 function Main() {
     return (
-        <>
-            <h1>Main Component</h1>
-        </>
+        <main>
+            <h1>Fun facts about React</h1>
+            <ul className="facts-list">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well over 100K stars on GitHub</li>
+                <li>Is maintained by Meta</li>
+                <li>Powers thousands of enterpise apps including mobile apps</li>
+            </ul>
+        </main>
     )
 }
 
